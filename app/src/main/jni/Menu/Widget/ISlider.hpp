4a3dfd5672da1bd8f2cb6e2047e1740598a28f13
duongnguyennalry-lang <duongnguyennalry@gmail.com>
2026-08-18 21:20:24 +0700

    Update ISlider.hpp

--- app/src/main/jni/Menu/Widget/ISlider.hpp
+++ app/src/main/jni/Menu/Widget/ISlider.hpp
@@ -1,11 +1,13 @@
 //
 // Created by github.com/seedhollow on 28/10/25.
+// Sửa lại theo yêu cầu chọn cách 1
 //
 
 #ifndef IL2CPPANDROID_ISLIDER_HPP
 #define IL2CPPANDROID_ISLIDER_HPP
 
 #include "BaseWidget.hpp"
+#include <algorithm> // Dùng cho std::clamp nếu cần kiểm tra giá trị
 
 class ISlider : public BaseWidget {
 public:
@@ -18,23 +20,35 @@ public:
      * pMin: Minimum value of the slider
      * pMax: Maximum value of the slider
      */
-    ISlider(int pId, const std::string& pName, int pMin, int pMax) {
+    ISlider(int pId, const std::string& pName, int pMin, int pMax)
+        : BaseWidget() // Khởi tạo phần lớp cơ sở trước
+        , minValue(pMin)
+        , maxValue(pMax)
+    {
+        // Đảm bảo min luôn nhỏ hơn hoặc bằng max, tự điều chỉnh nếu nhập sai
+        if (minValue > maxValue) {
+            std::swap(minValue, maxValue);
+        }
+
+        // Gán giá trị cho thành phần của lớp cơ sở
         id = pId;
         type = "ISlider";
         name = pName;
-        minValue = pMin;
-        maxValue = pMax;
     }
 
+    // Khai báo lại hàm ToJson rõ ràng, ghi rõ override/final
     json ToJson() const override {
         return {
-                {"id", id},
-                {"type", type},
-                {"name", name},
-                {"min", minValue},
-                {"max", maxValue}
+            {"id",    id},
+            {"type",  type},
+            {"name",  name},
+            {"min",   minValue},
+            {"max",   maxValue}
         };
     }
+
+    // Đảm bảo có hàm hủy ảo đúng chuẩn kế thừa
+    ~ISlider() override = default;
 };
 
 #endif //IL2CPPANDROID_ISLIDER_HPP
