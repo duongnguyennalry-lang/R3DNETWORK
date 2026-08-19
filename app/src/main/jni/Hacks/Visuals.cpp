79d8e4c058f303f15bc313cd49e178be51a25fec
duongnguyennalry-lang <duongnguyennalry@gmail.com>
2026-08-19 09:06:02 +0700

    Update Visuals.cpp

--- app/src/main/jni/Hacks/Visuals.cpp
+++ app/src/main/jni/Hacks/Visuals.cpp
@@ -10,7 +10,7 @@ void Visuals::Update(Draw draw, int screenWidth, int screenHeight) {
     //UnityResolve::UnityType::Camera *camera = UnityResolve::UnityType::Camera::GetMain();
     // Full documentation of UnityResolve.hpp
     // https://github.com/issuimo/UnityResolve.hpp
-    if (Vars::PlayerData.ESPCrosshair) {
+    if (Vars::PlayerData::ESPCrosshair) {
         DrawEspCrosshair(draw);
     }
 }
@@ -19,13 +19,17 @@ void Visuals::DrawEspCrosshair(Draw draw) {
     // Draw ESP here
     Unity::Color crosshair_color {0, 0, 0, 255};
 
-    if (Vars::PlayerData.CrosshairColor == 0) {
+    if (Vars::PlayerData::CrosshairColor == 0) {
         crosshair_color = Unity::Color(255, 0, 0, 255);
-    } else if (Vars::PlayerData.CrosshairColor == 1) {
+    } else if (Vars::PlayerData::CrosshairColor == 1) {
         crosshair_color = Unity::Color(0, 255, 0, 255);
     } else {
         crosshair_color = Unity::Color(0, 0, 255, 255);
     }
 
-    draw.DrawCrosshair(crosshair_color,Unity::Vector2(draw.getWidth() / 2.0f,draw.getHeight() / 2.0f),Vars::PlayerData.CrosshairSize * 10);
+    draw.DrawCrosshair(
+        crosshair_color,
+        Unity::Vector2(draw.getWidth() / 2.0f, draw.getHeight() / 2.0f),
+        Vars::PlayerData::CrosshairSize * 10
+    );
 }
